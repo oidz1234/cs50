d2bfbdc982d55ce44fda8bb06ebdc8b0540a60fc
Mark McNally <markamcnally@gmail.com>
2017-06-23 07:54:02 +0000

    finally fixed the printf statement not working

--- pset1/mario.c
+++ pset1/mario.c
@@ -3,21 +3,24 @@
 
 int n;
 int height;
-int main(void)
- 
- 
-{
+
+int main(void) {
     
 do
+
 {
+    
 
        height = get_int();
-       
+    
+    if (height < 2 || height >28)
+    
+    printf("you number needs to be >=2 you have put %i",height);    //why is this not printing at the correct time ?   
 }
 
-while (height < 2);
+while (height < 2 || height >28);
 
-    printf("you number needs to be >=2 you have put%i,",height);    //why is this not printing at the correct time ?
+    
 
       
   
