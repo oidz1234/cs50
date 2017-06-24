132fefbcc5b1783d62d245714ec17298e883faaf
Mark McNally <markamcnally@gmail.com>
2017-06-24 15:39:13 +0000

    repostiioned some braces

--- pset1/mario.c
+++ pset1/mario.c
@@ -8,36 +8,35 @@ int main(void) {
 
 
     printf("enter the height of your pyramid:\n");   
-do
 
-{
-    
-       height = get_int();      
+
+do { 
+      height = get_int();      
     
     if (height < 2 || height >28)       //if the user has entered the correct input
     
     printf("you number needs to be no smaller then 0 and no greater then 28 you have put %i!\n",height);      
-}
+   }
 
-    while (height < 2 || height >28);  //rerun get int until user gives a vaild number
+while (height < 2 || height >28);  //rerun get int until user gives a vaild number
   
   
          
-             for (int k = 0; k < height; k++)
-             {
-             printf(" ");
+             for (int k = 0; k < height; k++) {
+             
+                printf(" ");
              
              {
              
              
-             for  (int h = 0; h < k; h++)
+             for  (int h = 0; h < k; h++) {
              
-            {
+             
                 printf("#");
-            }
-                {
-                    printf("\n");
-                }
+             }
+             {
+                printf("\n");
+             }
            
              }  
             }
