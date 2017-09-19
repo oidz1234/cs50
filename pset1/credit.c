fe7abb0f56db82142b98a387cae2834f04d17373
oidz1234 <markamcnally@gmail.com>
2017-09-19 11:13:00 +0100

    removed commentout code

--- pset1/credit.c
+++ pset1/credit.c
@@ -4,21 +4,6 @@ long credit = 0;
 
 
 
-/*int get_digits(long i) {
-
-int c;  digit position 
-long n = i;
-
-while(n != 0) {
-	n /= 10;
-	c++;
-
-
-*/
-
-
-
-
 int main(void) {
 
 
@@ -44,28 +29,4 @@ printf("credit is %lli\n",  n);
 
 }
 }
-
-
-
-
-
-/*
-while (credit > 0) {
-int i;
-long credit arr[i];
-        arr[i]= credit % 10;
-	number /= 10;
-	i++	
-
-printf("credit is %lli\n", credit);
-printf("arr is %i\n", arr[i]);
-
-}
-*/
-
-
-
-
-
-
 
