fd2c22f46e27448e7a470bcc1b2f66b31c5bec57
oidz1234 <markamcnally@gmail.com>
2017-09-19 11:10:24 +0100

    added credit.c

---
+++ pset1/credit.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+long credit = 0;
+
+
+
+/*int get_digits(long i) {
+
+int c;  digit position 
+long n = i;
+
+while(n != 0) {
+	n /= 10;
+	c++;
+
+
+*/
+
+
+
+
+int main(void) {
+
+
+	printf("please enter your card number\n");
+
+
+	scanf("%lli", &credit);
+
+
+	printf("you have entered %lli\n", credit);
+
+
+
+int c; /* digit position */
+long n = credit;
+
+	while(n != 0) {
+	n /= 10;
+	c++;
+
+
+printf("credit is %lli\n",  n);
+
+}
+}
+
+
+
+
+
+/*
+while (credit > 0) {
+int i;
+long credit arr[i];
+        arr[i]= credit % 10;
+	number /= 10;
+	i++	
+
+printf("credit is %lli\n", credit);
+printf("arr is %i\n", arr[i]);
+
+}
+*/
+
+
+
+
+
+
+
