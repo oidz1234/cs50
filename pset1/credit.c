18c57712524184103637aaf290a7af81a8325535
oidz1234 <markamcnally@gmail.com>
2017-09-20 11:52:25 +0100

    trying to work out how to split the timesed digits

--- pset1/credit.c
+++ pset1/credit.c
@@ -38,12 +38,25 @@ int main(void)
 
 		
 		int added_value = (digit * 2);
+printf("if this is greater then 9 the while loop shoild happen %d\n", added_value);			
+	while(added_value > 9) {
+		int added_digit = added_value % 10;
+		added_value /=10;
+printf("does this say 1 ? %d\n", added_value);
+
+printf("THIS SHOULD DISPLAY THE ADDED_DIGIT SPLIT: %d\n", added_digit);
+}
+
+//need to split up the number.
+ /*	
+	
+		int sum_mutiplyed = added_value + added_value +
 	
 
 			if (sum_mutiplyed != added_value) {
 
-				
-
+					
+*/
 				
 printf("the sum mutiplyed is %d\n", sum_mutiplyed);
 
