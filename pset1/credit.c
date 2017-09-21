ae55d91c82973a49d23584c7827b153b15b2aede
oidz1234 <markamcnally@gmail.com>
2017-09-21 15:04:49 +0100

    I guess it works now, it however, does also print invalid when looking for a visa card

--- pset1/credit.c
+++ pset1/credit.c
@@ -17,9 +17,6 @@ int main(void) {
 	scanf("%lli", &credit);
 	
 	long initial_credit = credit;
-	printf("FIRST INITIAL CREDIt %lli\n", initial_credit);
-	
-	printf("you have entered %lli\n", credit);
 
 
 /*
@@ -33,8 +30,6 @@ int main(void) {
 		 digit = credit % 10;  //splits the number into digits
 
 		credit /= 10;
-	printf("the digit is %d\n", digit);
-       	printf("credit is %lli\n", credit);
 
 					
 		if (count ==  2) {  //need to preform operations on every other digit
@@ -70,10 +65,6 @@ while(added_value > 9) {
 
 	
 		 sum_mutiplied = added_value + added_digit + sum_mutiplied;
-	printf("addedvalue = %d\n", added_value);
-	printf("addeddigit = %d\n", added_digit);
-
-	printf("sum_mmutiplied %d\n", sum_mutiplied);
 					
 count = 0;
 } 
@@ -84,7 +75,6 @@ else if (count == 1 ) {  //add the digits that where not mutipled
  all_not_mutiplied_added = all_not_mutiplied_added + not_mutiplied;
 
 
-printf("all not mutiplied is %d\n", all_not_mutiplied_added);
 }
 
  
@@ -92,46 +82,45 @@ printf("all not mutiplied is %d\n", all_not_mutiplied_added);
 
 }
 int final_number = all_not_mutiplied_added + sum_mutiplied;
-printf("final number is %d\n", final_number);
 
 int ends_in_zero = final_number % 10;
-printf("ends in zero %d\n", ends_in_zero); 
 
 
 int amex_credit = initial_credit /  10000000000000; 
 int mc_credit = initial_credit / 100000000000000;
 int visa_credit13 = initial_credit / 1000000000000;
 int visa_credit16 = initial_credit / 1000000000000000;
-printf("amex credit = %d\n", amex_credit);
-printf("mc credit = %d\n", mc_credit);
-printf("visa13 = %d visa 16 = %d\n", visa_credit13, visa_credit16);
+
 
 if (ends_in_zero != 0) {
 	printf("INVALID\n");
-	printf("INVALID 0 %d\n", ends_in_zero);
 }
-else  if (amex_credit == 34 || amex_credit == 37 ) {
-printf("MCDEBUB@AMEX%d\n",mc_credit); 
-printf("AMEX\n");
+
+  if (amex_credit == 34 || amex_credit == 37 ) {
+	printf("AMEX\n");
 
 }
 
-else if (mc_credit == 51 || mc_credit == 52 || mc_credit == 53 || mc_credit == 54 || mc_credit == 55) {
-printf("MASTERCARD\n");
-printf("MC DEBUGCREDIT@MC%d\n",mc_credit);
+ if (mc_credit == 51 || mc_credit == 52 || mc_credit == 53 || mc_credit == 54 || mc_credit == 55) {
+	printf("MASTERCARD\n");
+
 }
 
-else if (visa_credit13 == 4) {
-printf("VISA\n");
+ if (visa_credit13 == 4) {
+	printf("VISA\n");
+printf("0debugshoudlsay0 0: %d\n", ends_in_zero);
+
 }
-else if (visa_credit16 == 4) {
-printf("VISA\n");
+
+ if (visa_credit16 == 4) {
+	printf("VISA\n");
+
+printf("0debugshoudlsay0 0: %d\n", ends_in_zero);
 } 
-else {
-printf("not an amex, mastercard or visa\n");
-}
 
 }
+
+
 
 
 
