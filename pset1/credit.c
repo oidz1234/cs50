66b014e9616cc16754a8a67eec0410f578dfab88
oidz1234 <markamcnally@gmail.com>
2017-09-21 15:10:17 +0100

    so I think this is almost done. I have added comments at appropite places and it prints everything correctly. THe only problem is that it says INVALID upon enetering a visa number, it does print visa as well.

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+/*
+ * this program prints out
+ * what type of number is 
+ * on a credit card, it does
+ * not completly work though
+ */
 
+#include <stdio.h>
+//global variables ftw
+/
 long credit = 0;
 int digit;
 int count = 0;
@@ -48,7 +56,7 @@ int main(void) {
  */
 	
 
-while(added_value > 9) {   
+	while(added_value > 9) {   
 		 added_digit = added_value % 10;
 		added_value /=10;
 
@@ -66,7 +74,7 @@ while(added_value > 9) {
 	
 		 sum_mutiplied = added_value + added_digit + sum_mutiplied;
 					
-count = 0;
+	       	count = 0;
 } 
 
 else if (count == 1 ) {  //add the digits that where not mutipled
@@ -83,7 +91,13 @@ else if (count == 1 ) {  //add the digits that where not mutipled
 }
 int final_number = all_not_mutiplied_added + sum_mutiplied;
 
-int ends_in_zero = final_number % 10;
+int ends_in_zero = final_number % 10; //for validation
+
+/* 
+ * the reason for the following is
+ * that the cards are decided based
+ * on their first few digits
+ */
 
 
 int amex_credit = initial_credit /  10000000000000; 
@@ -91,6 +105,7 @@ int mc_credit = initial_credit / 100000000000000;
 int visa_credit13 = initial_credit / 1000000000000;
 int visa_credit16 = initial_credit / 1000000000000000;
 
+//decided what card it is
 
 if (ends_in_zero != 0) {
 	printf("INVALID\n");
@@ -108,14 +123,12 @@ if (ends_in_zero != 0) {
 
  if (visa_credit13 == 4) {
 	printf("VISA\n");
-printf("0debugshoudlsay0 0: %d\n", ends_in_zero);
 
 }
 
  if (visa_credit16 == 4) {
 	printf("VISA\n");
 
-printf("0debugshoudlsay0 0: %d\n", ends_in_zero);
 } 
 
 }
